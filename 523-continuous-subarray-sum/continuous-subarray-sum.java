2c6012bba5fecbc6e2a11b9fdbcce08cc7c87d28
GAURAV MISHRA <2k25cse2514061@gmail.com>
2026-08-11 19:51:01 +0530

    Time: 23 ms (92.55%) | Memory: 140.1 MB (7.67%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -0,0 +1,34 @@
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += nums[i];
+
+            int rem = sum % k;
+
+            if (map.containsKey(rem)) {
+
+                int length = i - map.get(rem);
+
+                if (length >= 2) {
+                    return true;
+                }
+
+            } else {
+                map.put(rem, i);
+            }
+        }
+
+        return false;
+    }
+}
+
