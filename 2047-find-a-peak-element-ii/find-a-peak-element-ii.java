58b587f6c9fa6d0158601776cd9353c7f5cdb309
GAURAV MISHRA <2k25cse2514061@gmail.com>
2026-08-12 18:47:27 +0530

    Time: 8 ms (5.08%) | Memory: 119 MB (78.89%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,43 +1,30 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-
-        int m = mat.length;
-        int n = mat[0].length;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                int top = -1;
-                int bottom = -1;
-                int left = -1;
-                int right = -1;
-
-                if (i > 0) {
-                    top = mat[i - 1][j];
-                }
-
-                if (i < m - 1) {
-                    bottom = mat[i + 1][j];
-                }
-
-                if (j > 0) {
-                    left = mat[i][j - 1];
-                }
-
-                if (j < n - 1) {
-                    right = mat[i][j + 1];
-                }
-
-                if (mat[i][j] > top &&
-                    mat[i][j] > bottom &&
-                    mat[i][j] > left &&
-                    mat[i][j] > right) {
-
-                    return new int[]{i, j};
-                }
-            }
+    int m = mat.length;
+    int n = mat[0].length;
+    int [] ans = new int [2];
+    for(int i = 0;i<m;i++){
+    for(int j = 0;j<n;j++){
+       int top = -1; int left = -1;
+       int right = -1; int bottom = -1;
+       if(i>0){
+        top = mat[i-1][j];
+       }
+       if(j>0){
+        left = mat[i][j-1];
+       }
+       if(i<m-1){
+       bottom = mat[i+1][j];
+       }
+       if(j<n-1){
+        right = mat[i][j+1];
+       }
+        if(mat[i][j]>top&& mat[i][j]>bottom && mat[i][j]>left && mat[i][j]>right ){
+         ans[0] = i;  ans[1] = j;
+         return ans;
+        }  
         }
-
-        return new int[]{-1, -1};
+    } 
+return ans;
     }
 }
