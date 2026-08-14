192134dd3be3f80bd152d287939293f9bfe3e195
GAURAV MISHRA <2k25cse2514061@gmail.com>
2026-08-14 10:35:33 +0530

    Time: 7 ms (28.10%) | Memory: 109.7 MB (5.25%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,30 +10,38 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-       ListNode newlist = new ListNode(head.val);
-        ListNode traverse_newlist = newlist;
-        ListNode temp = head.next;
+    //    ListNode newlist = new ListNode(head.val);
+    //     ListNode traverse_newlist = newlist;
+    //     ListNode temp = head.next;
 
-        while(temp != null){
-            traverse_newlist.next = new ListNode(temp.val);
-            traverse_newlist = traverse_newlist.next;
-            temp = temp.next;
-        }
-
+    //     while(temp != null){
+    //         traverse_newlist.next = new ListNode(temp.val);
+    //         traverse_newlist = traverse_newlist.next;
+    //         temp = temp.next;
+    //     }
     ListNode curr = head;
     ListNode prev = null;
+    ListNode dummy = new ListNode(0);
+    ListNode temp = dummy;
+   
+    while(head!=null){
+        temp.next = new ListNode(head.val);
+        head = head.next;
+        temp = temp.next;
+    }
+   
     while(curr!=null){
         ListNode next = curr.next;
         curr.next = prev;
         prev = curr;
         curr = next;
     }
-    while(newlist!=null){
-    if(newlist.val!=prev.val){
+    while(dummy.next!=null){
+    if(dummy.next.val!=prev.val){
      return false;
     }
     else{
-        newlist = newlist.next;
+      dummy = dummy.next;
         prev = prev.next;
     }
     }
