d34e21f7ddc38f060627ba0aa066dd32462e4d54
GAURAV MISHRA <2k25cse2514061@gmail.com>
2026-08-21 10:35:42 +0530

    Time: 1 ms (98.32%) | Memory: 42.5 MB (79.25%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,16 +1,26 @@
 class Solution {
     public int mySqrt(int x) {
-     for(int i = 0;i<=x;i++){
-        if((long)i*i==x){
-            return i;
+        int low = 0;
+        int high = x-1;
+        if(x==1){
+            return 1;
         }
-        else if((long)i*i<x && (long)(i+1)*(i+1)>x){
-         return i;
+        while(low<=high){
+        int mid = low+(high-low)/2;
+        if((long)mid*mid==x){
+            return mid;
+        }
+         else if((long)mid*mid<x && (long)(mid+1)*(mid+1)>x){
+           return mid;
+        }
+        else if((long)mid*mid<x){
+        low = mid+1;
         }
         else{
-            continue;
+            high = mid-1;
         }
+       
      }
-    return -1;
+    return 0;
     }
 }
