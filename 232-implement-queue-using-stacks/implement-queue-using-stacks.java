0c0c156ea07ea8ae66a02ef186f26897276d28fa
GAURAV MISHRA <2k25cse2514061@gmail.com>
2026-09-05 19:29:55 +0530

    Time: 2 ms (17.85%) | Memory: 42.9 MB (32.48%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class MyQueue {
+    Stack<Integer> st1 = new Stack<>();
+    Stack<Integer> st2 = new Stack<>();
+
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+       
+        while(!st1.isEmpty()) {
+            st2.push(st1.pop());
+        }
+          st1.add(x);
+
+        while(!st2.isEmpty()) {
+            st1.push(st2.pop());
+        }
+    }
+    
+    public int pop() {
+        return st1.pop();
+    }
+    
+    public int peek() {
+        return st1.peek();
+    }
+    
+    public boolean empty() {
+        return st1.isEmpty();
+    }
+}
