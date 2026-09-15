66556e3b320b5f3d6bc37c84d1cbdfe76bedd8ac
GAURAV MISHRA <2k25cse2514061@gmail.com>
2026-09-15 19:30:42 +0530

    Time: 4 ms (51.20%) | Memory: 46.6 MB (36.23%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+    int n = s.length();
+    HashMap<Character,Integer> mp = new HashMap<>();
+    mp.put('I',1); 
+    mp.put('V',5);
+    mp.put('X',10);
+    mp.put('L',50); 
+    mp.put('C',100);
+    mp.put('D',500);
+    mp.put('M',1000);
+    int sum = 0;
+    for(int i = 0;i<n-1;i++){
+    int temp = mp.get(s.charAt(i));
+    int memp = mp.get(s.charAt(i+1));
+    if(temp>=memp){
+        sum+=temp;   
+    }
+    else{
+        sum-=temp;
+    }
+    }
+     sum+=mp.get(s.charAt(n-1));
+     return sum;  
+    }
+}
