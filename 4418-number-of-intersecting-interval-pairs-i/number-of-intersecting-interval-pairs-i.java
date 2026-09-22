5c7a7d04417bd7e637477bcf647a0776da109ecc
GAURAV MISHRA <2k25cse2514061@gmail.com>
2026-09-22 18:58:16 +0530

    Time: 4 ms (99.88%) | Memory: 46.7 MB (94.90%) - LeetSync

---
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countIntersectingIntervals(int[][] intervals) {
+     int n =  intervals.length;
+    
+     int count  = 0;
+     for(int i = 0;i<n-1;i++){
+        
+        for(int j = i+1;j<n;j++){
+        if(intervals[i][0]<=intervals[j][1] && intervals[i][1]>=intervals[j][0]){
+        count++;
+
+        }
+        }
+     }  
+     return count;
+    }
+}
